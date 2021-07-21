d1d7619c63e0b4df559329d97af1e029807b4d30
Matheus Loubake <mloubake@hotmail.com>
2021-07-21 17:59:50 -0300

    Adding Axios to the project to handle http requests. Added TMBD API Key to env.local. Starting connection to TMDB API.

--- movies-app-web/src/pages/Main.js
+++ movies-app-web/src/pages/Main.js
@@ -1,23 +1,31 @@
 import { useState } from "react";
 
+import api from "../services/api";
+
 import MovieCard from "../component/MovieCard/index";
 
 export default function Main() {
-  const [movies, setMovies] = useState([
-    { title: "movie 1", genre: "genre a", releaseYear: "XXXX" },
-    { title: "movie 2", genre: "genre b", releaseYear: "XXXX" },
-    { title: "movie 3", genre: "genre c", releaseYear: "XXXX" },
-  ]);
+  // const [movies, setMovies] = useState([
+  //   { title: "movie 1", genre: "genre a", releaseYear: "XXXX" },
+  //   { title: "movie 2", genre: "genre b", releaseYear: "XXXX" },
+  //   { title: "movie 3", genre: "genre c", releaseYear: "XXXX" },
+  // ]);
+
+  api
+    .get(`/movie/popular?api_key=${process.env.REACT_APP_TMDB_API_KEY}`)
+    .then((result) => {
+      console.log(result.data.results);
+    });
 
   return (
-    <div style={{ border: "5px dotted black" }}>
-      {movies.map((movie) => (
+    <div>
+      {/* {movies.map((movie) => (
         <MovieCard
           title={movie.title}
           genre={movie.genre}
           releaseYear={movie.releaseYear}
         />
-      ))}
+      ))} */}
     </div>
   );
 }
