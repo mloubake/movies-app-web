0ad9a2c999032f0cac63840917e37f5cee347026
Matheus Loubake <mloubake@hotmail.com>
2021-09-08 16:25:32 -0300

    Converting Genre List from MovieCard to text.

--- movies-app-web/src/component/MovieCard/index.js
+++ movies-app-web/src/component/MovieCard/index.js
@@ -1,6 +1,12 @@
 import "./styles.css";
 
 export default function MovieCard({ posterPath, title, genre, releaseYear }) {
+  function convertGenreListToText(genreList) {
+    const bulletPoint = " \u2022 ";
+
+    return genreList.join(bulletPoint);
+  }
+
   return (
     <>
       <div className="movie-item">
@@ -13,7 +19,7 @@ export default function MovieCard({ posterPath, title, genre, releaseYear }) {
         </div>
         <div className="movie-info">
           <span className="title">{title}</span>
-          <span className="genre">{genre}</span>
+          <span className="genre">{convertGenreListToText(genre)}</span>
           <span className="release-year">{releaseYear}</span>
           <button>+ detalhes</button>
         </div>
